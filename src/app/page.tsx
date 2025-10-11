7440aa7a2b94d0b1949cd04c4a39527c79890d7e
MonoToneMoe <gghaddadgg@gmail.com>
2025-10-11 12:53:13 -0700

    moved navbar to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Navbar from "@/components/navbar";
 import Events from "@/components/sections/events/events";
 import Hero from "@/components/sections/hero/hero";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <Navbar />
-      <main className="">
-        <Hero />
-        {/* <Events /> */}
-      </main>
-    </div>
+    <main>
+      <Hero />
+      {/* <Events /> */}
+    </main>
   );
 }
