8067958b115042380d8b8355e551199cecd7fd83
MonoToneMoe <gghaddadgg@gmail.com>
2025-10-18 16:52:41 -0700

    added navbar background controller to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { NavbarBackgroundController } from "@/components/layout/navbar-background-controller";
 import Events from "@/components/sections/events/events";
 import Hero from "@/components/sections/hero/hero";
 
 export default function Home() {
   return (
     <main>
+      <NavbarBackgroundController />
       <Hero />
       <Events />
     </main>
