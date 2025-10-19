47675a2976d40ce2d1e1ccb1c5a65014e04d55de
MonoToneMoe <gghaddadgg@gmail.com>
2025-10-19 14:27:12 -0700

    moved components to PageSections folder for home page and updated imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { NavbarBackgroundControllerObserver } from "@/components/layout/navbar-background-controller-observer";
-import Events from "@/components/sections/events/events";
-import Hero from "@/components/sections/hero/hero";
-import Projects from "@/components/sections/projects/projects";
+import Events from "@/components/PageSections/Home/events/events";
+import Hero from "@/components/PageSections/Home/hero/hero";
+import Projects from "@/components/PageSections/Home/projects/projects";
+import { getFormattedDate } from "@/lib/utils";
 
 export default function Home() {
   return (
     <main>
+      <p className="text-xl text-white">{getFormattedDate(new Date)}</p>
+
       <Hero />
       <Events />
       <Projects />
