6099c3bd0e4a4cf37d3ba07461e1b3e2409a9b5c
MonoToneMoe <gghaddadgg@gmail.com>
2025-10-19 14:34:07 -0700

    removed example code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,10 @@ import { NavbarBackgroundControllerObserver } from "@/components/layout/navbar-b
 import Events from "@/components/PageSections/Home/events/events";
 import Hero from "@/components/PageSections/Home/hero/hero";
 import Projects from "@/components/PageSections/Home/projects/projects";
-import { getFormattedDate } from "@/lib/utils";
 
 export default function Home() {
   return (
     <main>
-      <p className="text-xl text-white">{getFormattedDate(new Date)}</p>
-
       <Hero />
       <Events />
       <Projects />
