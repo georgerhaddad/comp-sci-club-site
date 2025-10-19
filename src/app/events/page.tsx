a9d4ea41d41ac4db54c684863b5660d54e14d108
MonoToneMoe <gghaddadgg@gmail.com>
2025-10-19 13:41:28 -0700

    added Page to name to differentiate it from components

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,12 +1,11 @@
 import { NavbarBackgroundControllerObserver } from "@/components/layout/navbar-background-controller-observer";
 
-export default function Events() {
+export default function EventsPage() {
   return (
     <main className="min-h-svh">
       <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
         Upcoming Events
       </h1>
-
       <NavbarBackgroundControllerObserver initialColor="bg-background" />
     </main>
   );
