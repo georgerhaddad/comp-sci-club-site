3a807a7924e678feef20fb1f2db6e2aa9fe4f763
MonoToneMoe <gghaddadgg@gmail.com>
2025-10-19 04:11:42 -0700

    started projects section for landing page

---
+++ src/components/sections/projects/projects.tsx
@@ -0,0 +1,11 @@
+const Projects = () => {
+  return (
+    <section className="container mx-auto h-screen px-4 lg:px-0">
+      <h2 id="projects" className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Club Projects
+      </h2>
+    </section>
+  );
+};
+
+export default Projects;
