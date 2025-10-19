221dfafacc8a8b29c0e92aef17c5cbb61365c9a8
MonoToneMoe <gghaddadgg@gmail.com>
2025-10-19 04:28:38 -0700

    added projects section and nav bg controller

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import { NavbarBackgroundController } from "@/components/layout/navbar-background-controller";
+import { NavbarBackgroundControllerObserver } from "@/components/layout/navbar-background-controller-observer";
 import Events from "@/components/sections/events/events";
 import Hero from "@/components/sections/hero/hero";
+import Projects from "@/components/sections/projects/projects";
 
 export default function Home() {
   return (
     <main>
-      <NavbarBackgroundController />
       <Hero />
       <Events />
+      <Projects />
+      <NavbarBackgroundControllerObserver initialColor="bg-secondary" />
     </main>
   );
 }
