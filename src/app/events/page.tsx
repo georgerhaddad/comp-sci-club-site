6adeead3aacfba5e3dd38ffb33b45b2940d4b2aa
MonoToneMoe <gghaddadgg@gmail.com>
2025-10-21 05:45:45 -0700

    created events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,11 +1,36 @@
 import { NavbarBackgroundControllerObserver } from "@/components/layout/navbar-background-controller-observer";
+import EventList from "@/components/PageSections/Events/events-list";
+import { EventCardSkeleton } from "@/components/shared/event-card-skeleton";
+import { Suspense } from "react";
 
 export default function EventsPage() {
+
   return (
     <main className="min-h-svh">
       <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
         Upcoming Events
       </h1>
+      <section className="container mx-auto h-screen px-4 lg:px-0">
+        <h2
+          id="events"
+          className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight"
+        >
+          Upcoming Events
+        </h2>
+        <section className="grid grid-cols-1 flex-wrap justify-center gap-4 sm:flex">
+          <Suspense
+            fallback={
+              <>
+                {Array.from({ length: 9 }).map((_, i) => (
+                  <EventCardSkeleton key={i} />
+                ))}
+              </>
+            }
+          >
+            <EventList />
+          </Suspense>
+        </section>
+      </section>
       <NavbarBackgroundControllerObserver initialColor="bg-background" />
     </main>
   );
