225f9645c71106428d6b5036770d4cf349344174
MonoToneMoe <gghaddadgg@gmail.com>
2025-10-21 05:51:58 -0700

    updated interfaces to include "I" at the start of their names

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,18 @@
-export interface Location {
+export interface ILocation {
   street: string;
   city: string;
   state: string;
   zip: number;
   country: string;
+}
+
+export interface IEvent {
+  title: string;
+  description: string;
+  dateStart: Date;
+  dateEnd?: Date;
+  location: ILocation
+  id: string;
+  src: string;
+  isFeatured: boolean;
 }
