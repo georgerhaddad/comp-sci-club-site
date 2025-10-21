44a097e65b9c7287cf4da30c82e67454351352df
MonoToneMoe <gghaddadgg@gmail.com>
2025-10-21 10:06:01 -0700

    fixed build error

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { mockEvents } from "../mockdata";
 
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
+export async function GET(_: Request, { params }: { params: Promise<{id: string}> }) {
   const { id } = await params;
   const event = await mockEvents.find((e) => e.id === id);
   if (!event) return NextResponse.json({ error: "Event not found" }, { status: 404 });
