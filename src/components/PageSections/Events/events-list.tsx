2fb453079fd016b7e4059478570955d16e6ed90f
MonoToneMoe <gghaddadgg@gmail.com>
2025-12-17 09:42:11 -0800

    updated event list component to use drizzle instead of mock api data

--- src/components/PageSections/Events/events-list.tsx
+++ src/components/PageSections/Events/events-list.tsx
@@ -1,19 +1,48 @@
-import EventCard from '@/components/shared/event-card';
-import { parseEvents } from '@/lib/utils';
+import EventCard from "@/components/shared/event-card";
+import { db, events, locations } from "@/server/db/schema";
+import { desc, eq } from "drizzle-orm";
 
 interface Props {
-  limit?: number
+  limit?: number;
 }
-export default async function EventList({limit}: Props) {
-  const res = await fetch(`http://localhost:3000/api/events?limit=${limit}`, { cache: "no-store" });
-  const events = await res.json();
+export default async function EventList({ limit }: Props) {
+  const rows = await db
+    .select({
+      id: events.id,
+      title: events.title,
+      description: events.description,
+      dateStart: events.dateStart,
+      dateEnd: events.dateEnd,
+      image: events.image,
+      isFeatured: events.isFeatured,
 
-  const parsedEvents = parseEvents(events);
+      location: {
+        street: locations.street,
+        city: locations.city,
+        state: locations.state,
+        zip: locations.zip,
+        country: locations.country,
+      },
+    })
+    .from(events)
+    .leftJoin(locations, eq(events.id, locations.eventId))
+    .orderBy(desc(events.dateStart))
+    .limit(limit ?? 10);
 
   return (
     <>
-      {parsedEvents.map((event) => (
-        <EventCard key={event.id} {...event} />
+      {rows.map((event) => (
+        <EventCard
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          description={event.description}
+          dateStart={new Date(event.dateStart)}
+          dateEnd={event.dateEnd ? new Date(event.dateEnd) : undefined}
+          src={event.image}
+          isFeatured={event.isFeatured ?? false}
+          location={event.location ?? null}
+        />
       ))}
     </>
   );
