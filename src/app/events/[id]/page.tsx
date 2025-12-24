75f9214a7686c2d374112b3bba7e47e2974b0550
MonoToneMoe <gghaddadgg@gmail.com>
2025-12-23 17:27:05 -0800

    added suspense and temporary fallback for EventSection

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,17 +1,23 @@
-"use cache"
 import { NavbarBackgroundControllerObserver } from "@/components/layout/navbar-background-controller-observer";
 import EventSection from "@/app/events/[id]/_components/event";
+import { Suspense } from "react";
 
 interface Props {
   id: string;
 }
 
-export default async function EventPage({ params }: { params: Promise<Props> }) {
+export default async function EventPage({
+  params,
+}: {
+  params: Promise<Props>;
+}) {
   const { id } = await params;
   return (
-    <main className="">
-      <EventSection id={id} />
-      <NavbarBackgroundControllerObserver initialColor="bg-background" />
+    <main>
+      <Suspense fallback={<div>loading...</div>}>
+        <EventSection id={id} />
+      </Suspense>
+      {/* <NavbarBackgroundControllerObserver initialColor="bg-background" /> */}
     </main>
   );
 }
