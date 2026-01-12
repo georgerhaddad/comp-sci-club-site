ebbb3e6a6a674b938bce00ea3d6f2e60fe4c9295
MonoToneMoe <gghaddadgg@gmail.com>
2026-01-11 20:51:36 -0800

    updated drizzle config to allow multiple schema files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { env } from '@/env';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/server/db/schema.ts',
+  schema: './src/server/db/schema/*.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: env.DATABASE_URL!,
