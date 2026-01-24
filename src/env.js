dba5b6ffd95a828d5ea20d45ed5f3c789004190d
MonoToneMoe <gghaddadgg@gmail.com>
2026-01-23 20:55:01 -0800

    added uploadthing token env

--- src/env.js
+++ src/env.js
@@ -10,6 +10,7 @@ export const env = createEnv({
     GITHUB_CLIENT_SECRET: z.string().min(1),
     BETTER_AUTH_SECRET: z.string().min(32),
     BETTER_AUTH_URL: z.url(),
+    UPLOADTHING_TOKEN: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.url(),
